b36a3625e51fb31ba220115a142f1b26f14f97a1
sravan2305 <gollapudivenkatasravankumar@gmail.com>
2022-01-09 08:36:31 +0530

    vocal for local init

---
+++ src/ServiceWorker/SwDev2.js
@@ -0,0 +1,50 @@
+const publicVapidKey = 'BIMambGJEnPp-WpOJ_wGpTVj4_fB8MVO6w3D36xhwPcFXxJinTNPEkKBXAlMifuWrfm3jR5DGJgaZnjUc8q_9t8';
+
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/-/g, '+')
+    .replace(/_/g, '/');
+ 
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+ 
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+
+if ('serviceWorker' in navigator) {
+  console.log('Registering service worker');
+
+  run().catch(error => console.error(error));
+}
+
+async function run() {
+  console.log('Registering service worker');
+  const registration = await navigator.serviceWorker.
+    register('/worker.js', {scope: '/'});
+  console.log('Registered service worker');
+
+  console.log('Registering push');
+  const subscription = await registration.pushManager.
+    subscribe({
+      userVisibleOnly: true,
+      
+      applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+    });
+  console.log('Registered push');
+
+  console.log('Sending push');
+  await fetch('/subscribe', {
+    method: 'POST',
+    body: JSON.stringify(subscription),
+    headers: {
+      'content-type': 'application/json'
+    }
+  });
+  console.log('Sent push');
+}
